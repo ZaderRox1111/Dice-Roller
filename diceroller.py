3c18f67f78f74970290bc175a2b91917ff56e955
Zachariah Derrick <72170300+ZaderRox1111@users.noreply.github.com>
2020-10-14 23:09:47 -0700

    Update diceroller.py

--- diceroller.py
+++ diceroller.py
@@ -1,7 +1,7 @@
 import random
 
 def main():
-    print('To use the roller, just input the number of sides that you want. No decimals. If you want to exit, just type "exit"')
+    print('To use the roller, just input the number of sides that you want. No decimals. To roll more than one dice at the same time, just a space between the numbers. If you want to exit, just type "exit"')
     
     is_done = False
   
@@ -12,13 +12,14 @@ def main():
         else:
             is_done = False
       
-        dice_type = int(dice_type)
-      
-        if dice_type <= 0:
-            is_done = False
-            print(f"A dice can't have {dice_type} sides")
-        else:
-            print(f"Your roll is: {roll_dice(dice_type)}")
+            dice_type = dice_type.split()
+            dice_type = list(map(int, dice_type))
+            
+            for dice in dice_type:
+                if dice_type <= 0:
+                    print(f"A dice can't have {dice_type} sides")
+                else:
+                    print(f"Your roll is: {roll_dice(dice_type)}")
         
 def roll_dice(dice_type):
     return random.randint(1, dice_type)
